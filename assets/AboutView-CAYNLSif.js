92a16968cf2e35d3e2d6b7c4969fa19f41eeba63
Jacob Chestnut <jacob.chestnut@fbitn.com>
2024-06-06 10:21:34 -0500

    Updates

---
+++ assets/AboutView-CAYNLSif.js
@@ -0,0 +1,1 @@
+import{_ as s,c as o,o as t,p as c,a as d,b as e}from"./index-CaSK5yrf.js";const _={},n=a=>(c("data-v-0069a5a7"),a=a(),d(),a),i={class:"about"},r=n(()=>e("div",null,[e("h3",null,"This is a user with a name and an email address.")],-1)),p=[r];function l(a,h){return t(),o("div",i,p)}const f=s(_,[["render",l],["__scopeId","data-v-0069a5a7"]]);export{f as default};
