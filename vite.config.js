4e9598ac0d2c597699275414a9ab24e014d2a615
Jacob Chestnut <jacob.chestnut@fbitn.com>
2024-06-06 10:33:58 -0500

    change base again

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  base: '/vuex-demo/',
+  base: '/',
   plugins: [vue()],
   resolve: {
     alias: {
