87c5010d045bb5b6feecc6f77edf693eabe800ff
Jacob Chestnut <jacob.chestnut@fbitn.com>
2024-06-06 10:14:44 -0500

    add gh-pages branch

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  base: '/REPONAME/',
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
